e4f0d0fdbab883351750507383995f901eb19760
nellyXinwei <nelsonalbajr@yahoo.com>
2020-08-22 01:12:42 +0800

    init

---
+++ Sources/2.DrawShape/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("Hello World\n");
+
+  printf("   |\n");
+  printf("  /|\n");
+  printf(" / |\n");
+  printf("/ _|\n");
+
+  return 0;
+}
+
+/*
+clang main.c -o main
+./main
+*/
---
+++ Sources/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("3. Variables\n");
+
+  printf("   |\n");
+  printf("  /|\n");
+  printf(" / |\n");
+  printf("/ _|\n");
+
+  return 0;
+}
+
+/*
+clang main.c -o main
+./main
+*/
